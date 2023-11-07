b9bfff2d546835519e6ba8a3a86b60ab102f0661
YuTengjing <ytj2713151713@gmail.com>
2023-11-07 21:33:30 +0800

    新增生成题解模版脚本

---
+++ scripts/new.ts
@@ -0,0 +1,30 @@
+import fs from 'node:fs/promises';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const title = process.argv[2];
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const project = resolve(__dirname, '..');
+
+const solutionsFolder = resolve(project, `solutions/${title}/`);
+const solution1 = resolve(solutionsFolder, `solution1.js`);
+
+const testsFolder = resolve(project, `tests`);
+const test = resolve(testsFolder, `${title}.test.js`);
+const genTestPromise = fs.writeFile(
+    test,
+    `import { expect, test } from 'vitest';
+import  from '../solutions/${title}/solution1';
+
+test('${title}', () => {
+
+});
+`,
+    'utf8',
+);
+
+const genSolutionPromise = fs
+    .mkdir(solutionsFolder, { recursive: true })
+    .then(() => fs.writeFile(solution1, '', 'utf8'));
+await Promise.all([genSolutionPromise, genTestPromise]);
