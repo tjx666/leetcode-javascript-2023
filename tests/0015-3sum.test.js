a7f65df67dbc4254b3e084e76731859390cb4d8e
YuTengjing <ytj2713151713@gmail.com>
2023-11-07 20:10:43 +0800

    删除多余 logs

--- tests/0015-3sum.test.js
+++ tests/0015-3sum.test.js
@@ -2,7 +2,6 @@ import { expect, test } from 'vitest';
 import threeSum from '../solutions/0015-3sum/solution1';
 
 test('0015-3sum', () => {
-    console.log(threeSum([-1, 0, 1, 2, -1, -4]));
     expect(threeSum([-1, 0, 1, 2, -1, -4])).deep.equals([
         [-1, -1, 2],
         [-1, 0, 1],
